c3aa86093f880399b0abccc440f31758badb8770
Martin <mortuen-r@web.de>
2014-04-10 11:13:48 +0200

    added leeres project mit sfml #yolo

---
+++ WischmopX/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Window;
+using SFML.Graphics;
+
+
+class Game
+{
+      public static void Main()
+    {
+        
+    }
+}
