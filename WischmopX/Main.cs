77eeac565cde3d6ae5cce96e8124f2af4e6306e8
xKro <xkrox@web.de>
2014-04-16 17:29:41 +0200

    Dennis scheiße wieder berichtigt

--- WischmopX/Main.cs
+++ WischmopX/Main.cs
@@ -7,10 +7,36 @@ using SFML.Window;
 using SFML.Graphics;
 
 
-class Game
-{
-      public static void Main()
+    class Game
     {
+        static RenderWindow win;
+
+        public static void Main()
+        {
+            // Erzeuge ein neues Fenster
+            win = new RenderWindow(new VideoMode(800, 600), "Mein erstes Fenster");
+            win.Closed += win_Closed;
+
+            Player p = new Player("std", new Vector2f(300, 200));
+
+            while (win.IsOpen())
+            {
+                win.Clear();
+                p.updatePlayer();
+                p.drawPlayer(win);
+                
+
+                win.DispatchEvents();
+                win.Display();
+            }
+
+        }
+
+        static void win_Closed(object sender, EventArgs e)
+        {
+            // Fenster Schließen
+            (sender as Window).Close();
+        }
         
     }
-}
+
